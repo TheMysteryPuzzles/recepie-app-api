44ff9283d3b9c569aaf99fdb87f2b852ac5ec81f
Admin <omerkhann024@gmail.com>
2019-08-25 22:19:27 +0500

    e

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    
+    def test_create_user_with_email_sucessful(self):
+        email = 'test@gmail.com'
+        password = 'test12345'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+        
+    
+    def test_new_user_email_normalized(self):
+        email="test@GMAIL.COM"
+        user = get_user_model().objects.create_user(email, 'test124')
+        
+        self.assertEqual(user.email, email.lower())
+        
+    
+    def test_new_user_invalid_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test1234')
+    
+    
+    def test_create_new_super_user(self):
+        user = get_user_model().objects.create_superuser(
+            'test@testt.com',
+            'test123'
+        )
+        
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
